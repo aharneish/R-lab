7e0e3564953097dbed7fcdbedfc339fbac181a62
Abburu Aharneish <99192645+aharneish@users.noreply.github.com>
2023-02-02 15:15:44 +0530

    R codes taught in class

---
+++ 22236-MDSC-201 datatypes-1.R
@@ -0,0 +1,29 @@
+#date 25-01-2023
+print("Hello World!")
+print('Hello World')
+x<-2
+y<-3
+print(x*y)
+print(x^y)
+print(sqrt(99))
+print(x-y)
+print(x+y)
+sin(x^y)
+ceiling(x)
+floor(y)
+print(x/y)
+# creating a vector apple
+apple<-c("red","green",'yellow')
+print(apple)
+print(class(apple))
+d<-c(34)
+e<-c(33,23,44,232,332)
+print(class(e))
+print(class(d))
+f<-e+d
+print(f)
+f<-e*d
+print(f)
+j<-charToRaw(apple)
+l1<-list(c(10,20,"j"),1000,"many")
+print(l1)
